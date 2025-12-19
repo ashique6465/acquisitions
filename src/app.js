f9096d5c9fd24926efae6d42d78e7b7c70f2dda4
ashique6465 <mdashiqueali71@gmail.com>
2025-12-19 23:32:25 +0530

    implemented authentication

--- src/app.js
+++ src/app.js
@@ -1,9 +1,35 @@
-import exress from 'express';
+import express from 'express';
+import logger from '#config/logger.js';
+import helmet from 'helmet'
+import morgan from  'morgan'
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
-const app = exress();
+import authRoutes from './routes/auth.routes.js'
+const app = express();
+
+app.use(helmet());
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }))
+app.use(cookieParser())
+
+app.use(morgan('combined',{ stream: { write: (message)=> logger.info(message.trim())}}))
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hellow from Acquisitions!');
+    logger.info('Hello from Acquisitions!')
+  res.status(200).send('Hello from Acquisitions!');
 });
+
+app.get('/health', (req, res)=>{
+    res.status(200).json({ status:'OK', timestamp: new Date().toISOString(), uptime: process.uptime() })
+})
+
+app.get('/api', (req, res) =>{
+    res.status(200).json({ message: 'Acquisitions API is running!'})
+})
+
+app.use('/api/auth', authRoutes);
 
 export default app;
