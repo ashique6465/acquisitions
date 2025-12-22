d0feca3b03d8ed363e86641cbde1cb3a0b9c0396
ashique6465 <mdashiqueali71@gmail.com>
2025-12-22 23:49:27 +0530

    Implement Arcjet secururity

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.routes.js'
+import securityMiddleware from '#middleware/security.middleware.js'
 const app = express();
 
 app.use(helmet());
@@ -16,7 +17,7 @@ app.use(express.urlencoded({ extended:true }))
 app.use(cookieParser())
 
 app.use(morgan('combined',{ stream: { write: (message)=> logger.info(message.trim())}}))
-
+app.use(securityMiddleware)
 app.get('/', (req, res) => {
     logger.info('Hello from Acquisitions!')
   res.status(200).send('Hello from Acquisitions!');
